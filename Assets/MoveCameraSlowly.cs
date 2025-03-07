bd6437e07259d5c4e4d3c592a7697a363d9a43ad
Lucas Fernandes <84829810+notluxca@users.noreply.github.com>
2025-03-06 21:01:43 -0300

    :update: Cena finalizada

--- Assets/MoveCameraSlowly.cs
+++ Assets/MoveCameraSlowly.cs
@@ -3,8 +3,16 @@ using UnityEngine;
 public class MoveCameraSlowly : MonoBehaviour
 {
     public float _cameraSpeed;
+    private Vector3 initialPos;
+
+    private void Start() {
+        initialPos = transform.position;
+    }
     
     private void FixedUpdate() {
         transform.position += new Vector3(_cameraSpeed * 1, 0, 0);
+        if(transform.position.x > 56){
+            transform.position = initialPos;
+        }
     }
 }
